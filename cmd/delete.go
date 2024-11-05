361c3129df4c2eaf97777d089aba2976d95ccb40
tommyshem <tommy>
2024-11-05 17:25:13 +0000

    add delete cmd to remove database

--- cmd/delete.go
+++ cmd/delete.go
@@ -1,22 +1,30 @@
 package cmd
 
 import (
+	"log"
+	"os"
+
 	"github.com/spf13/cobra"
+	"github.com/tommyshem/ogi/cmd/storage/bolt"
 )
+
+var delete bool
 
 // TODO implement the delete database file
 var deleteDB = &cobra.Command{
 	Use:   "delete",
-	Short: "delete the local offline database file on your system.",
+	Short: "Delete the local ogi offline database file on your system.",
 	Run: func(cmd *cobra.Command, args []string) {
-		//	filePath := Location()
+		err := os.Remove(bolt.Location())
+		if err != nil {
+			log.Fatal(err)
+		}
 	},
 }
 
-// init registers the list command with the root command and sets up flags on
+// init registers the delete command with the root command and sets up flags on
 // the list command.
 func init() {
 	RootCmd.AddCommand(deleteDB)
-	listCmd.Flags().BoolVarP(&raw, "raw", "r", false, "Show the raw JSON for these issues")
-	listCmd.Flags().StringVarP(&state, "state", "s", "open", "List issues by their state <all, closed, open>")
+	listCmd.Flags().BoolVarP(&delete, "delete", "d", false, "Delete the offline issues database from the local machine")
 }
