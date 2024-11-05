7b52623927b8d032ea43a9b84824285aa373eab2
tommyshem <tommy>
2024-11-05 19:58:51 +0000

    fix flag to wrong command

--- cmd/update.go
+++ cmd/update.go
@@ -11,12 +11,12 @@ var updateCmd = &cobra.Command{
 	Use:   "update",
 	Short: "update offline issues to local ogi database",
 	Run: func(cmd *cobra.Command, args []string) {
-
+		//TODO write update func
 	},
 }
 
 // init registers the version command with the root command.
 func init() {
 	RootCmd.AddCommand(updateCmd)
-	fetchCmd.Flags().BoolVarP(&updateAll, "all", "a", false, "Fetch issues from all the repo's stored in the offline database")
+	updateCmd.Flags().BoolVarP(&updateAll, "all", "a", false, "Fetch issues from all the repo's stored in the offline database")
 }
